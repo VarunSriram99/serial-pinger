95b9be86b9c4e14bf1acd338309fe0f1a18d7b67
S Varun <s.varun@bigbinary.com>
2024-01-07 16:11:09 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :group, only: [:create]
+  resources :groups, only: [:create]
   get '/health_check', to: 'health_check#show'
 end
