ca3eaaa0eec40409d629ad53dcc2d2226f61f868
S Varun <s.varun@bigbinary.com>
2024-01-07 16:02:07 +0530

    added health check

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class GroupsController < ApplicationController
+  def index
+    render status: :ok, json: { groups: Group.all }
+  end
+
   def create
     Group.create(person_params)
   end
